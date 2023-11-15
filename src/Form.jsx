73c631afa62981bc575a7c2c09084af51eacea27
Tak <takeshiseodev@gmail.com>
2023-11-14 23:47:26 -0500

    update: value property for single source of truth

--- src/Form.jsx
+++ src/Form.jsx
@@ -24,23 +24,27 @@ function Form(props) {
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
             />
             <input 
                 type='email'
                 placeholder='Email'
                 onChange={handleChange}
                 name='email'
+                value={formData.email}
             />
             <textarea
                 placeholder="Comments" 
                 onChange={handleChange}
                 name="comments"
+                value={formData.comments}
             />
         </form>
     )
